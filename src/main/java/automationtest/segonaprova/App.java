91ad047019a17263167441904d3a374ec99f830f
david <sndivad@gmail.com>
2020-05-17 22:06:36 +0200

    segonaprova - primer commit

---
+++ src/main/java/automationtest/segonaprova/App.java
@@ -0,0 +1,14 @@
+package automationtest.segonaprova;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("segona prova amb el git");
+    }
+}
